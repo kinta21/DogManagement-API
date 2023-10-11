863d291beea6623b0643ada84e1c9b780908526a
kinta21 <kinta21127@yahoo.ne.jp>
2023-10-11 20:06:37 +0900

    最初のコミット

---
+++ src/main/java/com/raisetech/dogmanagement/DogmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.raisetech.dogmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raisetech/dogmanagement/DogmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raisetech.dogmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
