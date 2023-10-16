ca597c08fead4e2ea23b25e95eb544b5f38264a8
kinta21 <kinta21127@yahoo.ne.jp>
2023-10-16 20:42:53 +0900

    Dogクラス修正

--- src/main/java/com/raisetech/dogmanagement/entity/Dog.java
+++ src/main/java/com/raisetech/dogmanagement/entity/Dog.java
@@ -1,14 +1,12 @@
 package com.raisetech.dogmanagement.entity;
 
-import java.util.Objects;
-
 public class Dog {
     private int id;
-    private  String name;
+    private String name;
     private String sex;
-    private  String age;
-    private  String dogBreed;
-    private  String region;
+    private String age;
+    private String dogBreed;
+    private String region;
 
     public Dog(int id, String name, String sex, String age, String dogBreed, String region) {
         this.id = id;
@@ -66,19 +64,5 @@ public class Dog {
     public void setRegion(String region) {
         this.region = region;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o)
-            return true;
-        if (o == null || getClass() != o.getClass())
-            return false;
-        Dog dog = (Dog) o;
-        return id == dog.id && Objects.equals(name, dog.name) && Objects.equals(sex, dog.sex) && Objects.equals(age, dog.age) &&Objects.equals(dogBreed, dog.dogBreed) &&Objects.equals(region, dog.region);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, sex, dogBreed, region);
-    }
 }
+
